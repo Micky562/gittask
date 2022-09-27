e13bf4453816ceebc9fc12f254c8d0fd4bd8be60
Micky562 <114070651+Micky562@users.noreply.github.com>
2022-09-27 10:22:35 +0530

    Add files via upload
    Q2

--- Program.java
+++ Program.java
@@ -1,36 +1,25 @@
-package code;
+package ttn;
+
+import java.util.Scanner;
 
 public class Program 
 {
- public static String FirstName; 
- public static String LastName;
- public static int Age;	
- static //static block will execute first then main method,static block is used to initialize static variable
- {
-	 FirstName="Micky";
-	 LastName="Kumar";
-	 Age=24;
- }
- public static void Display() //static method
- {
-	 System.out.println("FirstName: "+ FirstName);
-	 System.out.println("LastName: "+LastName);
-	 System.out.println("Age: "+Age);
- }
+ static Scanner sc=new Scanner(System.in);
  public static void main(String args[])
  {
-	System.out.println("Details are:"); 
-	System.out.println("FirstName: "+ FirstName);
-	System.out.println("LastName: "+LastName);
-	System.out.println("Age: "+Age);
-	System.out.println("-------Static method output-----------");
-	//below method will call static method Display
-	Program.Display();
-	Age=30;//Age changes here
-	System.out.println("-------------Accessing static variables----------------");
-	//we can access static variable using class name directly no need to create object
-	System.out.println("FirstName: "+FirstName); //we can also access with variable name because we are in same class 
-	System.out.println("LastName: "+Program.LastName);
-	System.out.println("Age: "+ Program.Age);
+	 StringBuilder st=new StringBuilder();
+	 while(true) 
+	 {
+        System.out.println("Enter your text");
+         st.append(sc.nextLine()); 
+         st.append("\n");
+         System.out.println("if done please type XDONE else type any anything");
+         String s=sc.nextLine();
+         if(s.equals("XDONE"))
+          break;   
+	 }
+	 System.out.println("Text enterd by the user");
+	 System.out.println(st);
  }
+ 
 }
